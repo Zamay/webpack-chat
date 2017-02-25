9b83452b170823bf1ff55756809b2e6d03ac0482
Zamay <mityazamay@gmail.com>
2017-02-25 21:30:19 +0200

    expand

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,55 @@
-$('.chat[data-chat=person2]').addClass('active-chat');
-$('.person[data-chat=person2]').addClass('active');
+window.onload = function () {
 
-$('.left .person').mousedown(function(){
-    if ($(this).hasClass('.active')) {
+    $('.chat[data-chat=person2]').addClass('active-chat');
+    $('.person[data-chat=person2]').addClass('active');
+
+    $('.left .person').mousedown(function () {
+        if ($(this).hasClass('.active')) {
+            return false;
+        } else {
+            var findChat = $(this).attr('data-chat');
+            var personName = $(this).find('.name').text();
+            $('.right .top .name').html(personName);
+            $('.chat').removeClass('active-chat');
+            $('.left .person').removeClass('active');
+            $('.write').show();
+            $(this).addClass('active');
+            $('.chat[data-chat = ' + findChat + ']').addClass('active-chat');
+        }
+    });
+
+    $(document).on('click', '.setting-user a', function () {
+        if ($(this).hasClass('.active')) {
+            return false;
+        } else {
+            var findChat = $(this).attr('data-chat');
+            var personName = 'Setting';
+            $('.right .top .name').html(personName);
+            $('.chat').removeClass('active-chat');
+            $('.write').hide();
+            $('.left .person').removeClass('active');
+            $('.chat[data-chat = ' + findChat + ']').addClass('active-chat heid');
+
+        }
+    });
+
+    $(document).on('click', '.search-icon a', function () {
+        $('.search').toggle();
+
+    });
+
+    $(document).on('click', '.expand a', function () {
+        // var compress = $('.compress');
+        // var expand = $('.expand');
+        // if (compress.style.display == "none"){
+        //     expand.show();
+        //     compress.hide();
+        // } else {
+        //     expand.hide();
+        //     compress.show();
+        // }
+        $('.right').toggleClass('add-width');
         return false;
-    } else {
-        var findChat = $(this).attr('data-chat');
-        var personName = $(this).find('.name').text();
-        $('.right .top .name').html(personName);
-        $('.chat').removeClass('active-chat');
-        $('.left .person').removeClass('active');
-        $(this).addClass('active');
-        $('.chat[data-chat = '+findChat+']').addClass('active-chat');
-    }
-});
+    });
+
+}
